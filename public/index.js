a5b3db7f2806e45226681ed02af3e2eb461087de
wagee <110701083+wajeeh-h@users.noreply.github.com>
2022-09-11 20:13:06 -0400

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
-window.addEventListener("load", async (event) => {
-  await tf
+window.addEventListener("load", (event) => {
+  tf
     .loadGraphModel(
       "https://download936.mediafire.com/hpl2wsq1mxrg/56q3xxl3tzo96jp/model.json"
     )
