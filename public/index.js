6813c400afe959e44ae748bf7bf957f2545494e7
wagee <110701083+wajeeh-h@users.noreply.github.com>
2025-02-10 19:34:49 -0500

    Delete public directory

--- public/index.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", (event) => {
-  tf
-    .loadGraphModel(
-      "https://download936.mediafire.com/hpl2wsq1mxrg/56q3xxl3tzo96jp/model.json"
-    )
-    .catch((error) => {
-      console.error(error);
-    });
-});
